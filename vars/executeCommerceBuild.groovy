636b24e64aaa069e92fc471aad0dc0f9c9421a8a
Mahesh Bishnoi <32988963+Mahesh-Bishnoi@users.noreply.github.com>
2021-11-18 13:47:08 +0530

    Update executeCommerceBuild.groovy

--- vars/executeCommerceBuild.groovy
+++ vars/executeCommerceBuild.groovy
@@ -1,6 +1,6 @@
 def call(commerceDir) {
 	echo "##### Execute install script ##### -- ${commerceDir}"
 	addProperty(commerceDir, "solrserver.instances.default.autostart=false")
-	sh "cd ${commerceDir}/core-customize/hybris/bin/platform && . ./setantenv.sh && ant clean all"
+	bat "cd ${commerceDir}/core-customize/hybris/bin/platform && setantenv.bat && ant clean all"
 } 
    
