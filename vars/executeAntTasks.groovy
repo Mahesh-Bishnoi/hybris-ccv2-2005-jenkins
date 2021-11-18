f29c7611bb7a2345e9365465bc9b4a017040fb93
Mahesh Bishnoi <32988963+Mahesh-Bishnoi@users.noreply.github.com>
2021-11-18 13:50:09 +0530

    Update executeAntTasks.groovy

--- vars/executeAntTasks.groovy
+++ vars/executeAntTasks.groovy
@@ -1,4 +1,4 @@
 def call(commerceDir, antTasks, env, antOptions = "-Xmx512m -Dfile.encoding=UTF-8") {
         echo "##### Execute ant tasks : ${antTasks} #####"
-        sh "cd ${commerceDir}/core-customize/hybris/bin/platform && . ./setantenv.sh && export ANT_OPTS=\"${antOptions}\" && ant ${antTasks} -Denv=${env}"
+        bat "cd ${commerceDir}/core-customize/hybris/bin/platform && setantenv.bat && export ANT_OPTS=\"${antOptions}\" && ant ${antTasks} -Denv=${env}"
 } 
