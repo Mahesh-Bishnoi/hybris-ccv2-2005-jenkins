4ddd3766a3494f307b903471b2e9b73d2743e3c2
Mahesh Bishnoi <32988963+Mahesh-Bishnoi@users.noreply.github.com>
2021-11-18 17:06:41 +0530

    Update extractCommerce.groovy

--- vars/extractCommerce.groovy
+++ vars/extractCommerce.groovy
@@ -1,10 +1,9 @@
 def call(commerceDir) {
-    def path = /\/core-customize/
     echo "##### Extract commerce platform ##### -- ${commerceDir}"
     //sh "unzip -o ../CXCOM2005*.ZIP -d ${commerceDir}/core-customize"
-    bat "cd ../ && 7z.exe x CXCOM2005*.ZIP -o${commerceDir}${path}"
-    bat "cd ../ && 7z.exe x CXCOMINTPK200500P*.ZIP -o${commerceDir}${path}"
-    bat "cd ../ && 7z.exe x hybrisConfig*.ZIP -o${commerceDir}${path}"
+    bat '''cd ../ && 7z.exe x CXCOM2005*.ZIP -o${commerceDir}\core-customize'''
+    bat '''cd ../ && 7z.exe x CXCOMINTPK200500P*.ZIP -o${commerceDir}\core-customize'''
+    bat '''cd ../ && 7z.exe x hybrisConfig*.ZIP -o${commerceDir}\core-customize'''
     /** Uncomment if you will be using the Integration Extension Pack
     echo "##### Extract commerce integration pack #####"
     sh "unzip -o ../CXCOMINTPK2005*.ZIP -d ${commerceDir}/core-customize"
